4673ad96b8347305a66ad5615ed857c8c2f32b19
Camael Ventura <camael.ventura@gmail.com>
2020-12-19 18:59:15 -0300

    port

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ import cors from 'cors';
 import routes from './routes';
 import './database';
 
+import 'dotenv/config';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.port || 3333);
